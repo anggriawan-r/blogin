95e909de1612a9e17c923a8327d77346abc50508
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2023-12-01 16:42:42 +0700

    add fetch to root page

--- src/app/_lib/getData.ts
+++ src/app/_lib/getData.ts
@@ -1,4 +1,4 @@
-async function getData() {
+export async function getData() {
   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/blog`, {
     cache: "no-store",
   });
--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,21 +1,25 @@
 import React, { Suspense } from "react";
 import Blog from "./Blog";
 import Link from "next/link";
+import { getData } from "@/app/_lib/getData";
+import { BlogListType } from "@/utils/types";
 
 export default async function BlogList() {
+  const data = await getData();
+
   return (
     <section
       id="blog"
       className="container my-24 flex scroll-mt-24 flex-col items-center justify-center gap-16 px-4"
     >
       <h1 className="text-center text-3xl font-bold">Recent Blogs</h1>
-      {/* <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<p>Loading...</p>}>
         <div className="grid grid-cols-1 gap-x-6 gap-y-16 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-12 ">
-          {data.map((data) => (
+          {data.map((data: BlogListType) => (
             <Blog key={data.id} content={data} />
           ))}
         </div>
-      </Suspense> */}
+      </Suspense>
       <Link href="/blog">
         <button className="btn w-[240px] border border-gray-900 bg-white text-gray-900 transition hover:bg-gray-900 hover:text-white">
           Load more
