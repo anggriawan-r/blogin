dc65ec9c4ac26c0a1e249a78abe8076a46ba6673
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2023-12-03 16:50:41 +0700

    Fix minor bug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import parse from "html-react-parser";
 import { BlogListType } from "@/utils/types";
 import Image from "next/image";
