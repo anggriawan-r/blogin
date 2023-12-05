779543a3e905df3cb30c36f5568decaa39bd1835
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2023-12-05 17:05:18 +0700

    Fetch failed, trying fixing it

--- src/app/user/blog/[uid]/page.tsx
+++ src/app/user/blog/[uid]/page.tsx
@@ -8,17 +8,17 @@ export default async function UserBlog({
 }: {
   params: { uid: string };
 }) {
-  const data: BlogType[] = await getBlogs(1, params.uid);
+  // const data: BlogType[] = await getBlogs(1, params.uid);
 
   return (
     <section className="container my-24 flex flex-col gap-16">
       <h1 className="text-center text-3xl font-bold">
-        {data[0].user.name}&apos;s Blogs
+        {/* {data[0].user.name}&apos;s Blogs */}
       </h1>
       <div className="grid grid-cols-1 gap-x-6 gap-y-16 px-4 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-12">
-        {data.map((d) => (
+        {/* {data.map((d) => (
           <Blog key={d.id} content={d} />
-        ))}
+        ))} */}
       </div>
     </section>
   );
